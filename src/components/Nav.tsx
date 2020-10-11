6795ce770ab89096bed1f905599e1288efb1759e
xuwenli <xuwenli0225.com>
2020-10-11 14:33:18 +0800

    svg图标问题解决

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg')
+require('icons/chart.svg')
+require('icons/tag.svg')
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -9,7 +12,15 @@ const NavWrapper = styled.nav`
     >li {
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        height: 24px;
+        width: 24px;
+      }
     }
   }
 `
@@ -19,12 +30,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
