bd1da214fa8d1988bd03811d894354dac32252f0
xuwenli <xuwenli0225.com>
2020-10-13 07:50:10 +0800

    Money.tsx之类型CategorySelection UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -45,7 +45,27 @@ const NotesSection = styled.section`
   }
 `
 const CategorySelection = styled.section`
-
+  font-size: 24px;
+  > ul {
+    display:  flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `
 const NumberPadSection = styled.section`
 
@@ -71,7 +91,7 @@ function Money() {
       </NotesSection>
       <CategorySelection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySelection>
