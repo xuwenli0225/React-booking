c522f1aea86c24e8b04a5a1dceb4fce24f6338ba
xuwenli <xuwenli0225.com>
2020-10-16 07:58:03 +0800

    Money.tsx标签的选择和新增

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,160 +1,33 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
+import { TagsSection } from 'views/Money/TagsSection'
+import { NotesSection } from 'views/Money/NotesSection'
+import { CategorySection } from 'views/Money/CategorySection'
+import { NumberPadSection } from 'views/Money/NumberPadSection'
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `
-const TagsSelection = styled.section`
-  background: #fff;
-  padding: 12px 16px;
-  flex-grow: 1; display: flex; flex-direction: column;
-  justify-content: flex-end; align-items: flex-start;
-  > ol {
-    margin: 0 -12px;
-    > li {
-      background: #d9d9d9; 
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-  > button {
-    background: none;
-    border: none;
-    padding: 2px 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
-    margin-top: 8px;
-  }
-`
-const NotesSection = styled.section`
-  background: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
-`
-const CategorySelection = styled.section`
-  font-size: 24px;
-  > ul {
-    display:  flex;
-    background: #c4c4c4;
-    > li {
-      width: 50%;
-      text-align: center;
-      padding: 16px 0;
-      position: relative;
-      &.selected::after {
-        content: '';
-        display: block;
-        height: 3px;
-        background: #333;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        left: 0;
-      }
-    }
-  }
-`
-const NumberPadSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output {
-    background: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0,0.25),
-                inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-  > .pad {
-    > button {
-      font-size: 10px;
-      float: left;
-      width: 25%;
-      height: 64px;
-      border: none;
-      &.ok{
-        height: 128px; float: right;
-      }
-      &.zero{
-        width: 50%;
-      }
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #bbb;
-      }
-      &:nth-child(12) {
-        background: #9a9a9a;
-      }
-      &:nth-child(14) {
-        background: #a9a9a9;
-      }
-    }
-  }
-`
-
 
 function Money() {
   return (
     <MyLayout>
-      <TagsSelection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSelection>
+      <TagsSection>
+
+      </TagsSection>
       <NotesSection>
         <label>
           <span>备注</span>
           <input type="text" placeholder="在这里提交备注" />
         </label>
       </NotesSection>
-      <CategorySelection>
+      <CategorySection>
         <ul>
           <li className="selected">支出</li>
           <li>收入</li>
         </ul>
-      </CategorySelection>
+      </CategorySection>
       <NumberPadSection>
         <div className="output">
           100
