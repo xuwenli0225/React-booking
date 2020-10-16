d0b433e3886bf05461e2ea61eb2002471bca3055
xuwenli <xuwenli0225.com>
2020-10-16 20:38:49 +0800

    Category切换支出和收入

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -14,19 +14,10 @@ function Money() {
   return (
     <MyLayout>
       <TagsSection>
-
       </TagsSection>
       <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里提交备注" />
-        </label>
       </NotesSection>
       <CategorySection>
-        <ul>
-          <li className="selected">支出</li>
-          <li>收入</li>
-        </ul>
       </CategorySection>
       <NumberPadSection>
         <div className="output">
--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-const CategorySection = styled.section`
+import React, { useState } from 'react'
+const Wrapper = styled.section`
   font-size: 24px;
   > ul {
     display:  flex;
@@ -22,4 +23,19 @@ const CategorySection = styled.section`
     }
   }
 `
+const CategorySection: React.FC = () => {
+  const categoryMap = { '-': '支出', '+': '收入' }
+  const [categoryList] = useState<('+' | '-')[]>(['-', '+'])
+  const [category, setCategory] = useState('-')
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li className={category === c ? 'selected' : ''}
+            onClick={() => { setCategory(c) }}
+          >{categoryMap[c]}</li>)}
+      </ul>
+    </Wrapper>
+  )
+}
 export { CategorySection }
