645b1092062da2d592e19f0ebf5854ed35781d07
xuwenli <xuwenli0225.com>
2020-10-19 00:22:28 +0800

    删除标签

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -25,8 +25,27 @@ const InputWrapper = styled.div`
   margin-top: 16px;
 `
 const Tag: React.FC = () => {
-  const { findTag, updateTag } = useTags()
+  const { findTag, updateTag, deleteTag } = useTags()
   let { id: idString } = useParams<Params>()
+  const tagContent = (tag: { id: number, name: string }) => (
+    <div>
+      <InputWrapper>
+        <Input label="标签名" value={tag.name} type="text" placeholder="标签名"
+          onChange={(e) => {
+            updateTag(tag.id, { name: e.target.value })
+          }}
+        />
+      </InputWrapper>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button onClick={() => {
+          deleteTag(tag.id)
+        }}>删除标签</Button>
+      </Center>
+    </div>
+  );
   const tag = findTag(parseInt(idString))
   return (
     <div>
@@ -36,19 +55,7 @@ const Tag: React.FC = () => {
           <span>编辑标签</span>
           <Icon />
         </Topbar>
-        <InputWrapper>
-          <Input label="标签名" value={tag.name} type="text" placeholder="标签名"
-            onChange={(e) => {
-              updateTag(tag.id, { name: e.target.value })
-            }}
-          />
-        </InputWrapper>
-        <Center>
-          <Space />
-          <Space />
-          <Space />
-          <Button>新增标签</Button>
-        </Center>
+        {tag ? tagContent(tag) : <Center>tag 不存在</Center>}
       </Layout>
     </div>
   )
