05feb66a5dcc8c1cde153055e6cd2759e60e39d9
xuwenli <xuwenli0225.com>
2020-10-18 23:34:15 +0800

    Tag.tsx样式完成

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useTags } from '../useTags'
 import { useParams } from 'react-router-dom'
 import { Button } from 'components/Button'
+import { Center } from 'components/Center'
+import { Space } from 'components/Space'
+import { Input } from 'components/Input'
 import Layout from 'components/Layout'
 import Icon from 'components/Icon'
 import styled from 'styled-components'
@@ -16,7 +19,12 @@ const Topbar = styled.header`
   padding: 14px;
   background: white;
 `
-const Tag: React.FC = (props) => {
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 16px;
+`
+const Tag: React.FC = () => {
   const { findTag } = useTags()
   let { id } = useParams<Params>()
   const tag = findTag(parseInt(id))
@@ -28,15 +36,15 @@ const Tag: React.FC = (props) => {
           <span>编辑标签</span>
           <Icon />
         </Topbar>
-        <div>
-          <label>
-            <span>标签名</span>
-            <input type="text" placeholder="标签名" />
-          </label>
-        </div>
-        <div>
-          <Button>删除标签</Button>
-        </div>
+        <InputWrapper>
+          <Input label="标签名" value={tag.name} type="text" placeholder="标签名" />
+        </InputWrapper>
+        <Center>
+          <Space />
+          <Space />
+          <Space />
+          <Button>新增标签</Button>
+        </Center>
       </Layout>
     </div>
   )
