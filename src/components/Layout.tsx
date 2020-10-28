f2334c96094f9f901b739c658a5d469395a8af51
xuwenli <xuwenli0225.com>
2020-10-29 07:56:09 +0800

    优化滚动逻辑

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Nav from './Nav'
 import styled from 'styled-components';
 
@@ -11,17 +11,30 @@ const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-
-const Layout = (props: any) => {
+type Props = {
+  className?: string;
+  scrollTop?: number;
+}
+const Layout: React.FC<Props> = (props) => {
+  const mainRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    setTimeout(() => {
+      if (!mainRef.current) { return; }
+      mainRef.current.scrollTop = props.scrollTop!
+    }, 0)
+  }, [props.scrollTop])
   return (
     <div>
       <Wrapper>
-        <Main className={props.className}>
+        <Main ref={mainRef} className={props.className} data-x={'lynn'}>
           {props.children}
         </Main>
         <Nav />
       </Wrapper>
     </div>
   )
+}
+Layout.defaultProps = {
+  scrollTop: 0
 }
 export default Layout
